3faefa960cdbd93d07e1a89b4fa8f7b75f99bc3b
Gutu <gutur.dev@gmail.com>
2025-10-24 11:12:42 +0300

    Hero setup

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const HeroSection = () => {
   return (
-    <div className="min-h-screen w-full">
+    <div className=" top-0 w-full object-center overflow-hidden flex items-center justify-center bg-background">
+      <div className="h-screen w-5/6 mask-x-from-60% mask-y-from-80% ">
+        
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HeroSection
+export default HeroSection;
--- src/pages/HomeView.tsx
+++ src/pages/HomeView.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import React from "react";
 import { useTheme } from "next-themes";
@@ -8,9 +8,13 @@ const HomeView = () => {
   const { setTheme } = useTheme();
   setTheme("dark");
   return (
-  <div className="w-full h-full inline-block">
-    <HeroSection />
-  </div>
-)};
+    <div className="w-full h-full inline-block bg-muted">
+      <div className="relative top-0 z-5">
+        <HeroSection />
+      </div>
+      <div className="relative h-screen z-10 bg-accent">Page contents here</div>
+    </div>
+  );
+};
 
 export default HomeView;
