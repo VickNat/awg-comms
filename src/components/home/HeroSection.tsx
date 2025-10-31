d83870a8b899a1cb76ad057ca4380f92b546b266
Gutu <gutur.dev@gmail.com>
2025-10-31 17:50:06 +0300

    Hero Title and catch phrase

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -2,9 +2,14 @@ import React from "react";
 
 const HeroSection = () => {
   return (
-    <div className=" top-0 w-full object-center overflow-hidden flex items-center justify-center bg-background">
-      <div className="h-screen w-5/6 mask-x-from-60% mask-y-from-80% ">
-        
+    <div className="relative w-full min-h-screen object-center overflow-hidden flex items-center justify-center bg-background">
+      <div className="flex flex-col gap-10">
+        <h1 className="w-full text-7xl uppercase font-bold tracking-wide">
+          awg communications
+        </h1>
+        <h2 className="text-3xl text-center ">
+          Creative, Passionate and Rooted in Story.
+        </h2>
       </div>
     </div>
   );
