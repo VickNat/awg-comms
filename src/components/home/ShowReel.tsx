a1b69414f5e04fc97eee85f562afe82887126cab
Gutu <gutur.dev@gmail.com>
2025-11-05 10:16:27 +0300

    hero text anim

--- src/components/home/ShowReel.tsx
+++ src/components/home/ShowReel.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const ShowReel = () => {
   return (
-    <div className="relative min-h-screen w-full flex items-center justify-center mt-24">
+    <div className="relative min-h-screen w-full flex items-center justify-center bg-black">
       <div className="relative min-h-screen w-full scale-75 bg-primary/50 flex items-center justify-start">
       <p className="absolute text-8xl uppercase text-accent font-bold w-full flex justify-center">play</p>
       <p className="absolute text-4xl uppercase text-accent font-bold self-end p-4">showreel</p>
