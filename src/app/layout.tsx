ea950b65fb58f796ce6d90092e92b8d96e83b61c
Gutu <gutur.dev@gmail.com>
2025-11-06 04:05:46 +0300

    Font and footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,18 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Arimo, Montserrat } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/lib/theme-provider";
-import Navbar from "@/components/layout/Navbar";
+// import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
 import { ReactLenis } from "@/utils/lenis";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const arimo = Arimo({
+  variable: "--font-arimo-mono",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const montserrat = Montserrat({
+  variable: "--font-montserrat-sans",
   subsets: ["latin"],
 });
 
@@ -30,7 +30,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <ReactLenis root>
         <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          className={`${arimo.variable} ${montserrat.variable} antialiased`}
         >
           <ThemeProvider
             attribute="class"
